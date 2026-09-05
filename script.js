2f2b0ec9f210cec2ea9fa91e2087cd209796c2eb
Amirkami33 <105497438+Amirkami33@users.noreply.github.com>
2026-09-05 23:47:59 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const player = document.getElementById("player");
 
+document.addEventListener("keydown", jump);
+
+document.addEventListener("touchstart", jump);
+
+function jump(){
+
+if(player.classList.contains("jump"))
+return;
+
+player.classList.add("jump");
+
+setTimeout(() => {
+player.classList.remove("jump");
+},700);
+
+}
